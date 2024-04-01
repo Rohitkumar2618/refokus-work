3b68e64233774139882384c0a1a6c221f2a8c100
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-04-01 17:16:30 +0530

    work component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 
 import Navbar from './components/Navbar'
+import Work from './components/Work'
+
+
+
+
 
 const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-900  font-["satoshi"] text-white'>
       <Navbar/>
+      <Work/>
     </div>
   )
 }
